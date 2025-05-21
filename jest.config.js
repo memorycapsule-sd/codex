e00e5b2123b5d92b6cc6de300da0d05aabca3ce6
memorycapsule-sd <sean@memoriafilms.com>
2025-05-21 14:12:48 -0700

    Update jest.config.js to fix Firebase issue

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(expo|react-native'
+      + '|@react-native'
+      + '|@react-navigation'
+      + '|firebase'            // ← transform any firebase/* package
+      + ')/)',
+  ],
 };
+
